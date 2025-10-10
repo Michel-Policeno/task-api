d06e71eb8d298d6d1fef616c6f563b2ba5ac19e3
Michel Policeno <michel.policeno@hotmail.com>
2025-10-10 00:10:11 -0300

    feat: created users

---
+++ todo-api/src/main/java/com/br/todoapi/todo_api/security/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package com.br.todoapi.todo_api.security;
+
+import com.br.todoapi.todo_api.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        return new UserDetailsImpl(user);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getTipo().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsuario();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getAtivo();
+    }
+}
